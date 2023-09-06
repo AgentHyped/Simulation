75d4c0fa8a2457ee3a2e6f4b6dd45985d20d35f3
AgentHyped <86356767+AgentHyped@users.noreply.github.com>
2023-09-06 18:28:01 +0100

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    UserID: { type: String },
+    Money: { type: Number, default: 0 },
+    Inventory: { type: Object },
+    LastUpdated: { type: Date, default: new Date() }
+});
+
+module.exports = mongoose.model('Simulation', UserSchema);
