a64abbc63339764173a352ee748760778ccf7386
AgentHyped <86356767+AgentHyped@users.noreply.github.com>
2024-01-07 20:01:48 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     UserID: { type: String },
     Money: { type: Number, default: 0 },
     Inventory: { type: Object },
-    LastUpdated: { type: Date, default: new Date() }
+    LastUpdated: { type: Date, default: new Date() },
+    CooldownExpires: { type: Date, default: new Date() }
 });
 
 module.exports = mongoose.model('Simulation', UserSchema);
